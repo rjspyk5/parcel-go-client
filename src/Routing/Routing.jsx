91324f484c87b62825c7519cdf06c132383ee767
rjspyk5 <rjspyk5@gmail.com>
2024-06-02 04:20:53 +0600

    make dashboard route for different user

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -3,12 +3,20 @@ import { Root } from "../Layout/Root";
 import { ErrorPage } from "@/components/ErrorPage/ErrorPage";
 import { Login } from "@/components/Login/Login";
 import { Registration } from "@/components/Registration/Registration";
+import { Home } from "@/Pages/Home/Home";
+import path from "path";
+import { AdminDashboard } from "@/Pages/Dashboard/AdminDashboard/AdminDashboard";
+import { Dashboard } from "@/Layout/Dashboard";
+import { UserDashboard } from "@/Pages/Dashboard/UserDashboard/UserDashboard";
+import { DeliveryHeroDashboard } from "@/Pages/Dashboard/DevliveryHeroDashboard/DeliveryHeroDashboard";
+import { PrivateRoute } from "@/components/privateRoute/PrivateRoute";
 export const Routing = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
     children: [
+      { path: "/", element: <Home /> },
       {
         path: "/login",
         element: <Login />,
@@ -16,6 +24,28 @@ export const Routing = createBrowserRouter([
       {
         path: "/reg",
         element: <Registration />,
+      },
+    ],
+  },
+  {
+    path: "dashboard",
+    element: (
+      <PrivateRoute>
+        <Dashboard />
+      </PrivateRoute>
+    ),
+    children: [
+      {
+        path: "admin",
+        element: <AdminDashboard />,
+      },
+      {
+        path: "user",
+        element: <UserDashboard />,
+      },
+      {
+        path: "deliveryhero",
+        element: <DeliveryHeroDashboard />,
       },
     ],
   },
