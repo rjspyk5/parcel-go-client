ba544671e3017567c44ea36f90aabdd84a6f538d
rjspyk5 <rjspyk5@gmail.com>
2024-06-04 18:47:56 +0600

    Search button added on banner section

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -16,9 +16,31 @@ export const Banner = () => {
                 Build your new <span className="text-blue-400">Saas</span>{" "}
                 Project
               </h1>
-              <button className="w-full px-5 py-2 mt-4 text-sm font-medium text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-                Start project
-              </button>
+              <section className="relative w-full max-w-md px-5 py-4 mx-auto rounded-md">
+                <div className="relative">
+                  <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                    <svg
+                      className="w-5 h-5 text-gray-400"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                    >
+                      <path
+                        d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></path>
+                    </svg>
+                  </span>
+
+                  <input
+                    type="text"
+                    className="w-full py-3 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+                    placeholder="Search"
+                  />
+                </div>
+              </section>
             </div>
           </div>
         </div>
