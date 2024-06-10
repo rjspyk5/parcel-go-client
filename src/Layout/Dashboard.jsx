35fb90ba807982f6d9a07892c1f532a0f5148618
rjspyk5 <rjspyk5@gmail.com>
2024-06-11 02:00:12 +0600

    add navbar on admin panel

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,8 +1,20 @@
 import { useRoleCheker } from "@/Hooks/useRoleCheker";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, Link } from "react-router-dom";
+import { useState } from "react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+
+import { useAuth } from "@/Hooks/useAuth";
+
+import logo from "../assets/image/update image/logo2.png";
+import { ProfileDropDown } from "@/components/ProfileDropDown/ProfileDropDown";
+import { ToogleTheme } from "@/components/Theme/ToogleTheme";
 
 export const Dashboard = () => {
   const [role, isLoading] = useRoleCheker();
+  const [open, setOpen] = useState(false);
+  const { user } = useAuth();
   const userMenu = (
     <>
       <ul>
@@ -53,18 +65,40 @@ export const Dashboard = () => {
       {isLoading ? (
         "Loading......"
       ) : (
-        <div className="flex">
-          <div className="w-[20%] min-h-screen bg-gray-500">
-            {role === "user"
-              ? userMenu
-              : role === "deliveryHero"
-              ? deliveryHeroMenu
-              : adminMenu}
+        <>
+          {" "}
+          <div className="sticky top-0 z-50">
+            <div className="flex justify-between bg-accent  backdrop-blur-xl py-[9px]  ">
+              {/* left side  */}
+              <div className="flex   items-center ">
+                <Link
+                  to="/"
+                  className="flex rounded-md pr-2 hover:bg-accent font-bold text-lg  items-center "
+                >
+                  <img className="w-10 h-10" src={logo} alt="logo" />
+                  ParcelGo
+                </Link>
+              </div>
+              {/* end side */}
+              <div className="flex  gap-2 items-center">
+                <ToogleTheme />
+                <ProfileDropDown user={user} />
+              </div>
+            </div>
           </div>
-          <div className="overflow-hidden flex-grow w-full">
-            <Outlet></Outlet>
+          <div className="flex">
+            <div className="w-[20%] bg-accent min-h-screen shadow-2xl">
+              {role === "user"
+                ? userMenu
+                : role === "deliveryHero"
+                ? deliveryHeroMenu
+                : adminMenu}
+            </div>
+            <div className="overflow-hidden flex-grow w-full">
+              <Outlet></Outlet>
+            </div>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
