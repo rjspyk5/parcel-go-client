01a6e3f5433a09e33716947fe6bcd16eaf9f28d4
rjspyk5 <rjspyk5@gmail.com>
2024-06-20 01:49:35 +0600

    my reviews section card desgin complete

--- src/Pages/Dashboard/DevliveryHeroDashboard/MyReviews.jsx
+++ src/Pages/Dashboard/DevliveryHeroDashboard/MyReviews.jsx
@@ -1,13 +1,18 @@
 import { useAxiosSequre } from "@/Hooks/useAxiosSequre";
 import { useRoleCheker } from "@/Hooks/useRoleCheker";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Rating, Star } from "@smastrom/react-rating";
 import { useEffect, useState } from "react";
 
 export const MyReviews = () => {
   const [userInfo] = useRoleCheker();
   const axiosSequre = useAxiosSequre();
   const [reviews, setreviews] = useState([]);
-
+  const myStyles = {
+    itemShapes: Star,
+    activeFillColor: "#f15a25",
+    inactiveFillColor: "#f15b2533",
+  };
   useEffect(() => {
     userInfo &&
       axiosSequre
@@ -20,18 +25,49 @@ export const MyReviews = () => {
     <div className="p-4">
       <h1 className="text-center font-bold text-3xl mb-4">My Reviews</h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 mg:grid-cols-3 lg:grid-cols-4 gap-2">
-        <div className="border shadow-xl p-2 rounded-md">
-          <div className="flex gap"></div>
-          <Avatar>
-            <AvatarImage
-              src="https://github.com/shadcn.png"
-              className="w-9 h-9 rounded-full"
-              alt="user"
-            />
-            <AvatarFallback>user</AvatarFallback>
-          </Avatar>
-        </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 mg:grid-cols-3  gap-2">
+        {reviews.map((el) => {
+          return (
+            <div
+              key={el._id}
+              className=" bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden"
+            >
+              <div className="p-6">
+                <div className="flex items-center space-x-4">
+                  <Avatar>
+                    <AvatarImage
+                      src={el.reviewGiverImage}
+                      className="w-9 h-9 rounded-full"
+                      alt="user"
+                    />
+                  </Avatar>
+                  <div>
+                    <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200">
+                      {el.reviewGiverName}
+                    </h2>
+                    <p className="text-sm text-gray-500 dark:text-gray-400">
+                      {el.reviewGivenDate}
+                    </p>
+                  </div>
+                </div>
+                <div className="mt-4">
+                  <Rating
+                    readOnly
+                    spaceBetween="medium"
+                    itemStyles={myStyles}
+                    className="w-36"
+                    value={el.rating}
+                  />
+                </div>
+                <div className="mt-7">
+                  <q className="text-gray-700 dark:text-gray-300">
+                    {el.feedback}
+                  </q>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
