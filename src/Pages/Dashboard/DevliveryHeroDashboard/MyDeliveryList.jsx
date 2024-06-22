59b85b36167ca7d6254c22da4647ea90cadbce5c
rjspyk5 <rjspyk5@gmail.com>
2024-06-22 18:00:06 +0600

    my deliverylist action and refetch implement

--- src/Pages/Dashboard/DevliveryHeroDashboard/MyDeliveryList.jsx
+++ src/Pages/Dashboard/DevliveryHeroDashboard/MyDeliveryList.jsx
@@ -1,12 +1,14 @@
-import { useAuth } from "@/Hooks/useAuth";
 import { useAxiosSequre } from "@/Hooks/useAxiosSequre";
 import { useRoleCheker } from "@/Hooks/useRoleCheker";
 import { ModalForMap } from "@/components/ModalForMap/ModalForMap";
 import { Button } from "@/components/ui/button";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import Swal from "sweetalert2";
 
 export const MyDeliveryList = () => {
   const [role] = useRoleCheker();
+  const [loading, setloading] = useState(false);
   const axiosSequre = useAxiosSequre();
   const { data, isLoading, refetch } = useQuery({
     queryKey: ["assignedHeroDeliveries"],
@@ -16,11 +18,40 @@ export const MyDeliveryList = () => {
     },
   });
   const handleManageDelivery = async (status, id) => {
-    console.log(id);
-    const result = await axiosSequre.patch(`/booking/${id}`, {
-      status: status,
-    });
-    console.log(result.data);
+    let text;
+    if (status === "delivered") {
+      text = "marked it as delivered";
+    } else {
+      text = "cancel this delivery";
+    }
+    Swal.fire({
+      title: "Are you sure?",
+      text: `You want to ${text}`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#2cc951",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes",
+    })
+      .then(async (result) => {
+        if (result.isConfirmed) {
+          setloading(true);
+          const result = await axiosSequre.patch(`/booking/${id}`, {
+            status: status,
+          });
+          return result;
+        }
+      })
+      .then((result) => {
+        if (result?.data?.acknowledged) {
+          refetch();
+          setloading(false);
+          Swal.fire({
+            text: `Sucessfully ${text}`,
+            icon: "success",
+          });
+        }
+      });
   };
   return (
     <div className=" flex justify-center items-center">
@@ -95,7 +126,11 @@ export const MyDeliveryList = () => {
                               {el.recieverDeliveryAdress}
                             </td>
                             <td className="px-4 py-2 whitespace-nowrap text-xs md:text-sm text-gray-500 dark:text-gray-400">
-                              {el.reqDeliveryDate}
+                              {
+                                new Date(el?.reqDeliveryDate)
+                                  .toISOString()
+                                  .split("T")[0]
+                              }
                             </td>
                             <td className="px-4 py-2 whitespace-nowrap text-xs md:text-sm text-gray-500 dark:text-gray-400">
                               {el.approxDeliveryDate}
@@ -108,34 +143,52 @@ export const MyDeliveryList = () => {
                               />
                             </td>
                             <td className="px-4 py-2 whitespace-nowrap text-xs md:text-sm">
-                              <div className="flex items-center gap-x-2">
-                                <span
-                                  onClick={() =>
-                                    handleManageDelivery("delivered", el?._id)
-                                  }
-                                >
-                                  <Button
-                                    size="sm"
-                                    variant="outline"
-                                    className="bg-green-500 text-white hover:text-white hover:bg-green-600"
-                                  >
-                                    Delivery
-                                  </Button>
+                              {el.status === "delivered" ? (
+                                <span className="px-2 py-1 rounded-md text-green-600 bg-[#55e25546]">
+                                  Delivered
                                 </span>
-                                <span
-                                  onClick={() =>
-                                    handleManageDelivery("canceled", el?._id)
-                                  }
-                                >
-                                  <Button
-                                    size="sm"
-                                    variant="outline"
-                                    className="bg-red-500 text-white hover:text-white hover:bg-red-600"
-                                  >
-                                    Cancel
-                                  </Button>
+                              ) : el.status === "canceled" ? (
+                                <span className="px-2 py-1 rounded-md text-red-600 bg-[#e73c1d1a]">
+                                  Canceled
                                 </span>
-                              </div>
+                              ) : (
+                                <>
+                                  <div className="flex items-center gap-x-1">
+                                    <span
+                                      onClick={() =>
+                                        handleManageDelivery(
+                                          "delivered",
+                                          el?._id
+                                        )
+                                      }
+                                    >
+                                      <Button
+                                        size="sm"
+                                        variant="outline"
+                                        className="bg-green-500 text-white hover:text-white hover:bg-green-600"
+                                      >
+                                        Delivery
+                                      </Button>
+                                    </span>
+                                    <span
+                                      onClick={() =>
+                                        handleManageDelivery(
+                                          "canceled",
+                                          el?._id
+                                        )
+                                      }
+                                    >
+                                      <Button
+                                        size="sm"
+                                        variant="outline"
+                                        className="bg-red-500 text-white hover:text-white hover:bg-red-600"
+                                      >
+                                        Cancel
+                                      </Button>
+                                    </span>
+                                  </div>
+                                </>
+                              )}
                             </td>
                           </tr>
                         ))}
--- src/components/Loading/SearchSpinner.jsx
+++ src/components/Loading/SearchSpinner.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { MagnifyingGlass } from "react-loader-spinner";
 
 export const SearchSpinner = () => {
