807082288aa73e2585930b40276e9fd26eb78f4a
rjspyk5 <rjspyk5@gmail.com>
2024-06-23 04:41:19 +0600

    delivery section responsive

--- src/Pages/Dashboard/DevliveryHeroDashboard/MyDeliveryList.jsx
+++ src/Pages/Dashboard/DevliveryHeroDashboard/MyDeliveryList.jsx
@@ -58,8 +58,8 @@ export const MyDeliveryList = () => {
       <div className="w-full py-8 px-2 sm:px-4 lg:px-6">
         <div className="bg-white dark:bg-gray-900 overflow-hidden shadow-md rounded-lg">
           <div className="bg-white dark:bg-gray-900 overflow-hidden shadow-md rounded-t-lg">
-            <div className="p-6 bg-accent border-b border-gray-200 dark:border-gray-700">
-              <h1 className="text-3xl font-bold text-gray-900 dark:text-white text-center">
+            <div className="md:p-6 p-3 bg-accent border-b border-gray-200 dark:border-gray-700">
+              <h1 className="md:text-3xl text-xl font-bold text-gray-900 dark:text-white text-center">
                 My Delivery List
               </h1>
               <p className="mt-2 text-sm text-gray-600 dark:text-gray-400 text-center">
